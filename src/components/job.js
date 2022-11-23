141c313afe8143d15e8e854a827b588fa353af1b
veronica <v.escobar1@uniandes.edu.co>
2022-11-22 23:45:05 -0500

    Reto: agregue a la tabla una nueva columan que represente el numero de visitas a la oferta. Reto: Incluya la palabra millohn en los valores que representan el salario. Reto: Remplace todas las cadenas estaticas por componentes de formated message. Reto: Modifique la cadena millon por millones respectivamente cuando el idioma en el navegador sea español.

--- src/components/job.js
+++ src/components/job.js
@@ -1,5 +1,6 @@
 import React from "react";
-import {FormattedDate} from 'react-intl';
+import {FormattedDate, FormattedNumber} from 'react-intl';
+import {FormattedMessage,FormattedPlural} from 'react-intl';
 
 const Job = (props) => {
   return (
@@ -7,7 +8,14 @@ const Job = (props) => {
       <th scope="row">{props.offer.id}</th>
       <td>{props.offer.name}</td>
       <td>{props.offer.company}</td>
-      <td>{props.offer.salary}</td>
+      <td>
+        {props.offer.salary + "&ensp"}
+        <FormattedPlural>
+          value={props.offer.salary}
+          one={<FormattedMessage id="million"/>}
+          other={<FormattedMessage id="millions"/>}
+        </FormattedPlural>  
+      </td>
       <td>{props.offer.city}</td>
       <td>
         <FormattedDate
@@ -17,6 +25,9 @@ const Job = (props) => {
           day='numeric'
           weekday='long'
         />
+      </td>
+      <td>
+        <FormattedNumber value={props.offer.views}/>
       </td>
     </tr>
   );
