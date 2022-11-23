23d3231181efdc3a5981c4c3d4967b0b5faa68fc
veronica <v.escobar1@uniandes.edu.co>
2022-11-22 23:32:41 -0500

    Tutorial basico redo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {IntlProvider} from 'react-intl';
+import localeEsMessages from "./locales/es";
 
 
 import JobsList from "./components/jobsList";
 
 ReactDOM.render(
-        <IntlProvider locale="en">
+        <IntlProvider locale="es-ES" messages= {localeEsMessages}>
                 <JobsList/>
         </IntlProvider>, document.getElementById("root")
 );
