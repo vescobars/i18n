75d2bd12dbd66556f09d23286de41b937ca02c92
veronica <v.escobar1@uniandes.edu.co>
2022-11-22 23:36:06 -0500

    Reto: Determine dinamicamente el idioma del navegador. Check

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {IntlProvider} from 'react-intl';
-import localeEsMessages from "./locales/es";
-
 
 import JobsList from "./components/jobsList";
 
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
+
+//Select the locale dynamicaly
+const userLocale = (navigator.language && navigator.languages.length) ? navigator.languages[0] : navigator.language;
+
 ReactDOM.render(
-        <IntlProvider locale="es-ES" messages= {localeEsMessages}>
+        <IntlProvider locale={userLocale} messages= {localeEsMessages}>
                 <JobsList/>
         </IntlProvider>, document.getElementById("root")
 );
