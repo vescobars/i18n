2e2a36db957427ed0f8d875b71546a2be390325f
veronica <v.escobar1@uniandes.edu.co>
2022-11-22 23:31:46 -0500

    Tutorial basico redo

--- src/components/job.js
+++ src/components/job.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {FormattedDate} from 'react-intl';
 
 const Job = (props) => {
   return (
@@ -8,7 +9,15 @@ const Job = (props) => {
       <td>{props.offer.company}</td>
       <td>{props.offer.salary}</td>
       <td>{props.offer.city}</td>
-      <td>{props.offer.date}</td>
+      <td>
+        <FormattedDate
+          value={new Date(props.offer.date)}
+          year='numeric'
+          month='long'
+          day='numeric'
+          weekday='long'
+        />
+      </td>
     </tr>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {IntlProvider} from 'react-intl';
 
-import JobsList from "./components/jobslist";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+import JobsList from "./components/jobsList";
+
+ReactDOM.render(
+        <IntlProvider locale="en">
+                <JobsList/>
+        </IntlProvider>, document.getElementById("root")
+);
